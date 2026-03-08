bb09eec1988c53a0b7596214ffb6b2d63290c644
Miguel <miguelbedmards@gmail.com>
2026-03-08 19:52:21 +0100

    Se ha comenzado con el metodo cifradoCesar

--- mensajecesar/src/mensajecesar/CifradoCesar.java
+++ mensajecesar/src/mensajecesar/CifradoCesar.java
@@ -3,22 +3,26 @@ package mensajecesar;
 import java.util.Scanner;
 
 public class CifradoCesar {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// El programa utilizara algoritmo de cesar
-		Scanner sc = new Scanner(System.in);
 
 	}
 
 	public String cifradoCesar(String text, int llave) {
-		StringBuilder cifrado=new StringBuilder();
-		
-		
-		
-		
-		
-		
-		
+		StringBuilder cifrado = new StringBuilder();
+
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+			
+			if (Character.isLetter(c)) {
+				char pie= Character.isUpperCase(c) ? 'A':'a';
+			}
+			
+			
+		}
+
 		return "";
 	}
 }
