b982707f4cc9873c1794a4e58782b682ef103adf
Miguel <miguelbedmards@gmail.com>
2026-03-08 20:07:21 +0100

    Se ha añadido un comentario explicativo

--- mensajecesar/src/mensajecesar/CifradoCesar.java
+++ mensajecesar/src/mensajecesar/CifradoCesar.java
@@ -11,16 +11,29 @@ public class CifradoCesar {
 	}
 
 	public String cifradoCesar(String text, int llave) {
-		StringBuilder cifrado = new StringBuilder();
+		StringBuilder cifrado = new StringBuilder(); // Creacion del StringBuilder
+		/*
+		 * Antes de nada en este caso no podemos utilizar un for Each ya que estamos
+		 * recorriendo texto que no es un array de texto si queremos recorrer texto
+		 * array tendremos que convertirlo con .tocharArray pero aun sonando raro el for
+		 * tradicional es mejor en este caso que el for Each por respuesta Una vez
+		 * recorrido la variable text condicinamos que si character es la letra c
+		 * entonces que la convierta en mayñuscula el interrogante actua como tercer
+		 * operador que le indica al programa que ?'letra'es correcto : 'LETRA' es
+		 * incorrecto
+		 */
 
 		for (int i = 0; i < text.length(); i++) {
 			char c = text.charAt(i);
-			
+
 			if (Character.isLetter(c)) {
-				char pie= Character.isUpperCase(c) ? 'A':'a';
+				char pie = Character.isUpperCase(c) ? 'A' : 'a';
+
+				int posOr = c - pie;
+				int posDesplazada = ((posOr + llave));
+				char letracod=(char) (pie+posDesplazada);
 			}
-			
-			
+
 		}
 
 		return "";
